8a38455b279786c368622a53f5b10b1939228938
Chevannese <158519006+Chevannese@users.noreply.github.com>
2024-11-03 00:20:41 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+let shopItemsdata = [{
+    id:"product1",
+    name:"GG01840",
+    price:2400,
+    desc: "The Gucci Unisex Eyewear, manufactured by Kering Eyewear in 2018, is a timeless and versatile accessory that combines sophistication with practicality. Featuring a black full-rim square frame, this pair of glasses is made from durable acetate, offering a sleek and classic design that suits both oval and round face shapes. The clear lenses are customizable, allowing wearers to adapt them to their specific needs while maintaining a polished appearance..",
+    img:'https://image4.cdnsbg.com/2/10/419867_1599515255854.jpg'
+},
+{
+    id:"product2",
+    name:"GG12220",
+    price:47000,
+    desc: "With Gucci GG1222O glasses, vision correction is no longer an ugly essential item, but a fashion forward accessory that will enhance your style and bring a look together. Finely constructed from Metal , the Gold coloured frames will look great for any occasion. Easily add RX prescription lenses that are custom-made for you to your order for unbeatable prices.",
+    img:'https://image4.cdnsbg.com/2/10/610525_1688966471608.jpg'
+
+},
+{
+    id:"product3",
+    name:"GG10850",
+    price:41000,
+    desc: "The classic Gucci GG1085O are a fantastic choice for glasses. With a range of frame shapes which are finely crafted from Acetate , colours Shiny Transparent Azure and lens Customisable they are suitable for any wearer. At SmartBuyGlasses , we offer special deals on premium prescription lenses to go with your Shiny Transparent Azure frames.",
+    img:'https://image4.cdnsbg.com/2/10/573503_1660662396934.jpg'
+},
+{
+    id:"product4",
+    name:"GG13180",
+    price:34000,
+    desc: "The Gucci Women's Eyewear from 2023, manufactured by Kering Eyewear, is a stylish and eco-conscious choice for modern fashion lovers. The square frame shape complements oval and round face shapes, offering a bold yet elegant look.",
+    img:'https://image4.cdnsbg.com/2/10/626754_1694180735440.jpg'
+    
+},
+{
+    id:"product5",
+    name:"GG13200",
+    price:34000,
+    desc: "Gucci ’s GG1320O appear with a dynamic Eco-Acetate eyeglass frame. The SmartBuyGlasses prescription lenses option is a perfect alternative for when your eyeglasses happen to arrive on your doorstep, ready to try on. Most of our lenses are coated with a unique and advanced coating, which provides them with an anti-glare and scratch resistant cover. This product features Tortoise frame and standard lenses.",
+    img:'https://image4.cdnsbg.com/2/10/626760_1693562907628.jpg'
+    
+},
+{
+    id:"product6",
+    name:"GG1474OJ",
+    price:34000,
+    desc: "The classic Gucci Gucci GG1474OJ Asian Fit are a fantastic choice for glasses. With a range of frame shapes which are finely crafted from Acetate , colours Tortoise and lens Customisable they are suitable for any wearer.",
+    img:'https://image4.cdnsbg.com/2/10/657438_1700573529023.jpg'
+    
+},
+{
+    id:"product7",
+    name:"GG0737O",
+    price: 34000,
+    desc: "The Gucci Men's Eyewear, manufactured by Kering Eyewear in 2021, presents a modern and sleek design with its shiny transparent grey full-rim square frame. Made from durable acetate, the frame offers both style and longevity, while the bold square shape is particularly suited for oval and round face shapes, creating a sharp and defined look.",
+    img:'https://image4.cdnsbg.com/2/10/573400_1676538104637.jpg'
+    
+},
+{
+    id:"product8",
+    name:"GG0737O",
+    price:39000,
+    desc: "This season, the Gucci GG0737O glasses are a fashionable favourite from the Gucci eyeglasses collection. For optimal clarity in vision, we offer professional prescription RX lens to match your Transparent Green frames made from Acetate.",
+    img:'https://image4.cdnsbg.com/2/10/653787_1705232982434.jpg'
+    
+},
+{
+    id:"product9",
+    name:"GG1150O",
+    price:40000,
+    desc: "Try on the GG1150O eyeglasses without going to the store with our Virtual 3D Try On system that lets you digitally try on our collection of eyewear. Expertly crafted from Customisable , the eyeglasses in Gold compliments a range of different facial shapes and can match many outfits.",
+    img:'https://image4.cdnsbg.com/2/10/599462_1660850881977.jpg'
+    
+}
+
+]
